161132803e88871c0d070f0b38bf264fce9a5cc2
Calen <lokera6@gmail.com>
2020-02-11 21:01:10 -0500

    Hardware button works

--- serial_parser.m
+++ serial_parser.m
@@ -1,5 +1,5 @@
 %% Initialize serial port
-s = serial("COM6", "BaudRate", 9600);
+s = serial("COM3", "BaudRate", 9600);
 fclose(s)
 fopen(s)
 
@@ -10,9 +10,8 @@ commandwindow
 while 1
     msg = fscanf(s);
     [irp, usp, potp, but] = parse_message(msg);
-  %  cmd.position = [irp usp potp];
- %   robot.set(cmd);
-    disp(but);
+    cmd.position = [irp usp potp];
+    robot.set(cmd);
 end
 
 function [ir, us, pot, but] = parse_message(msg)
@@ -23,15 +22,15 @@ function [ir, us, pot, but] = parse_message(msg)
         irp = cell2mat(split_vals(1));
         usp = cell2mat(split_vals(2));
         potp = cell2mat(split_vals(3));
-        butp = cell2mat(split_vals(3));
+        butp = cell2mat(split_vals(4));
 
         % Map to 2*pi range
         irp = str2double(irp) * 2 * pi / 60;
         usp = str2double(usp) * 2 * pi / 120;
         potp = 2*pi - str2double(potp) * 2 * pi / 1023;
         butp = str2double(butp);
-        
-        disp("ir: " + irp + "   us: " + usp + "  pot: " + potp)
+        disp("ir: " + irp + "   us: " + usp + ...
+            "  pot: " + potp + " butp: " + butp)
     catch
     end
     ir = irp;
